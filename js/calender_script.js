b50e766c9d96d366ac0360044bb247058354c09e
sbonelosth <31245826+sbonelosth@users.noreply.github.com>
2020-12-27 09:11:51 +0200

    did some things.

--- js/calender_script.js
+++ js/calender_script.js
@@ -7,79 +7,79 @@ let select_month = document.getElementById("month");
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let month_year_dom = document.querySelector("#month-year");
+
+// Calling the calendar function
 showCalendar(current_month, current_year);
 
-
 function next() {
-    current_year = (current_month === 11) ? current_year + 1 : current_year;
-    current_month = (current_month + 1) % 12;
-    showCalendar(current_month, current_year);
+  current_year = (current_month === 11) ? current_year + 1 : current_year;
+  current_month = (current_month + 1) % 12;
+  showCalendar(current_month, current_year);
 }
 
 function previous() {
-    current_year = (current_month === 0) ? current_year - 1 : current_year;
-    current_month = (current_month === 0) ? 11 : current_month - 1;
-    showCalendar(current_month, current_year);
+  current_year = (current_month === 0) ? current_year - 1 : current_year;
+  current_month = (current_month === 0) ? 11 : current_month - 1;
+  showCalendar(current_month, current_year);
 }
 
 function jump() {
-    current_year = parseInt(select_year.value);
-    current_month = parseInt(select_month.value);
-    showCalendar(current_month, current_year);
+  current_year = parseInt(select_year.value);
+  current_month = parseInt(select_month.value);
+  showCalendar(current_month, current_year);
 }
 
 function showCalendar(month, year) {
 
-    let first_day = (new Date(year, month)).getDay();
-    let month_days = 32 - new Date(year, month, 32).getDate();
+  let first_day = (new Date(year, month)).getDay();
+  let month_days = 32 - new Date(year, month, 32).getDate();
 	
-    // Body of the calendar.
-    let tbl = document.getElementById("calendar-body");
+	// Body of the calendar.
+  let tbl = document.getElementById("calendar-body");
 
-    // Clearing all previous cells.
-    tbl.innerHTML = "";
+  // Clearing all previous cells.
+  tbl.innerHTML = "";
 
-    // Filling data about month and year in the page via DOM.
+  // Filling data about month and year in the page via DOM.
 
-    month_year_dom.innerHTML = "<span class='month'>" + months[month].toUpperCase() + "</span>" + " " + year;
-    select_year.value = year;
-    select_month.value = month;
+  month_year_dom.innerHTML = "<span class='month'>" + months[month].toUpperCase() + "</span>" + " " + year;
+  select_year.value = year;
+  select_month.value = month;
 
-    // Creating all cells.
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
+  // Creating all cells.
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
 	
-        // Creating a table row.
-        let row = document.createElement("tr");
+    // Creating a table row.
+    let row = document.createElement("tr");
 
-        // Creating individual cells & filling them up with data.
-        for ( let j = 0; j < 7; j++ ) {
-            if ( i === 0 && j < first_day ) {
-                let cell = document.createElement("td");
-                let cell_txt = document.createTextNode("");
-                cell.appendChild(cell_txt);
-                row.appendChild(cell);
-            }
-			
-            else if ( date > month_days ) {
-                break;
-            }
+    // Creating individual cells & filling them up with data.
+    for ( let j = 0; j < 7; j++ ) {
+      if ( i === 0 && j < first_day ) {
+        let cell = document.createElement("td");
+        let cell_txt = document.createTextNode("");
+        cell.appendChild(cell_txt);
+        row.appendChild(cell);
+      } 
+      else if ( date > month_days ) {
+        break;
+      }
 
-            else {
-                let cell = document.createElement("td");
-                let cell_txt = document.createTextNode(date);
+      else {
+        let cell = document.createElement("td");
+        let cell_txt = document.createTextNode(date);
 				
-                // Color today's date.
-                if ( date === today.getDate() && year === today.getFullYear() && month === today.getMonth() ) {
-                    cell.classList.add("today-bg"); 
-                }
-                cell.appendChild(cell_txt);
-                row.appendChild(cell);
-                date++;
-            }
+				// Color today's date.
+        if ( date === today.getDate() && year === today.getFullYear() && month === today.getMonth() ) {
+          cell.classList.add("today-bg"); 
         }
-		
-        // Finally, appending each row into calendar body.
-        tbl.appendChild(row);
+        cell.appendChild(cell_txt);
+        row.appendChild(cell);
+        date++;
+      }
     }
+		
+		// Finally, appending each row into calendar body.
+    tbl.appendChild(row);
+  }
 }
