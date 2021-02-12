6a7d200ec7651a0e5524835bb6f8be215edde879
sbonelosth <31245826+sbonelosth@users.noreply.github.com>
2021-02-12 17:57:24 +0200

    Add files via upload

--- js/calender_script.js
+++ js/calender_script.js
@@ -33,8 +33,8 @@ function showCalendar(month, year) {
 
   let first_day = (new Date(year, month)).getDay();
   let month_days = 32 - new Date(year, month, 32).getDate();
-	
-	// Body of the calendar.
+
+  // Body of the calendar.
   let tbl = document.getElementById("calendar-body");
 
   // Clearing all previous cells.
@@ -49,37 +49,174 @@ function showCalendar(month, year) {
   // Creating all cells.
   let date = 1;
   for (let i = 0; i < 6; i++) {
-	
+
     // Creating a table row.
     let row = document.createElement("tr");
 
     // Creating individual cells & filling them up with data.
-    for ( let j = 0; j < 7; j++ ) {
-      if ( i === 0 && j < first_day ) {
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < first_day) {
         let cell = document.createElement("td");
         let cell_txt = document.createTextNode("");
         cell.appendChild(cell_txt);
         row.appendChild(cell);
-      } 
-      else if ( date > month_days ) {
+      }
+      else if (date > month_days) {
         break;
       }
 
       else {
         let cell = document.createElement("td");
         let cell_txt = document.createTextNode(date);
-				
-				// Color today's date.
-        if ( date === today.getDate() && year === today.getFullYear() && month === today.getMonth() ) {
-          cell.classList.add("today-bg"); 
+
+        // Color today's date and label events.
+        if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+          // January, February event labels.
+          if (today.getDate() === 1 && today.getMonth() === 0) {
+            document.querySelector(".event-label").innerHTML = "Happy New Year, Make It Count!";
+          }
+          else if (today.getDate() === 21 && today.getMonth() === 0) {
+            document.querySelector(".event-label").innerHTML = "Happy Birthday, Rapsody!";
+          }
+          // February event labels.
+          else if (today.getDate() === 14 && today.getMonth() === 1) {
+            document.querySelector(".event-label").innerHTML = "Happy Valentine's Day!";
+          }
+          else if (today.getDate() === 23 && today.getMonth() === 1) {
+            document.querySelector(".event-label").innerHTML = "Happy Birthday, Little Simz!";
+          }
+          // March event labels.
+          else if (today.getDate() === 21 && today.getMonth() === 2) {
+            document.querySelector(".event-label").innerHTML = "Happy Human Rights Day!";
+          }
+          else if (today.getDate() === 22 && today.getMonth() === 2 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "Human Rights Day observed!";
+          }
+          else if (today.getDate() === 31 && today.getMonth() === 2) {
+            document.querySelector(".event-label").innerHTML = "The end of first quarter of the year!";
+          }
+          // April event labels.
+          else if (today.getDate() === 2 && today.getMonth() === 3 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "Good Friday!";
+          }
+          else if (today.getDate() === 3 && today.getMonth() === 3 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "Holy Saturday!";
+          }
+          else if (today.getDate() === 4 && today.getMonth() === 3 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "Easter Sunday!";
+          }
+          else if (today.getDate() === 5 && today.getMonth() === 3 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "Happy Family Day, Make It Count!";
+          }
+          else if (today.getDate() === 27 && today.getMonth() === 2) {
+            document.querySelector(".event-label").innerHTML = "Happy Freedom Day!";
+          }
+          // May event labels.
+          else if (today.getDate() === 1 && today.getMonth() === 4) {
+            document.querySelector(".event-label").innerHTML = "Worker's Day!";
+          }
+          else if (today.getDate() === 9 && today.getMonth() === 4 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "Happy Mother's Day!";
+          }
+          // June event labels.
+          else if (today.getDate() === 16 && today.getMonth() === 5) {
+            document.querySelector(".event-label").innerHTML = "Happy Youth Day!";
+          }
+          else if (today.getDate() === 20 && today.getMonth() === 5 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "Happy Father's Day!";
+          }
+          // July, August event labels.
+          else if (today.getDate() === 9 && today.getMonth() === 7) {
+            document.querySelector(".event-label").innerHTML = "Happy Women's Day!"
+          }
+          else if (today.getDate() === 9 && today.getMonth() === 7 && today.getFullYear() === 2021) {
+            document.querySelector(".event-label").innerHTML = "National Women's Day observed!"
+          }
+          // December event labels.
+          else if (today.getDate() === 16 && today.getMonth() === 11) {
+            document.querySelector(".event-label").innerHTML = "A Day of Reconciliation!";
+          }
+          else if (today.getDate() === 25 && today.getMonth() === 11) {
+            document.querySelector(".event-label").innerHTML = "Merry Christmas!";
+          }
+          else if (today.getDate() === 26 && today.getMonth() === 11) {
+            document.querySelector(".event-label").innerHTML = "A Day of Goodwill!";
+          }
+          else if (today.getDate() === 31 && today.getMonth() === 11) {
+            document.querySelector(".event-label").innerHTML = "Happy New Year's Eve!";
+          }
+          else {
+            document.querySelector(".event-label").innerHTML = "Just another day!"
+          }
+          cell.classList.add("today-bg");
+        }
+        // January events.
+        else if (date === 1 && month === 0) {
+          cell.classList.add("new-year");
+        }
+        // February, March events
+        else if (date === 21 && month === 2) {
+          cell.classList.add("human-rights");
+        }
+        else if ((date === 14 && month === 1) || (date === 22 && month === 2 && year === 2021)) {
+          cell.classList.add("observed");
+        }
+        // April events
+        else if ((date === 2 || date === 3 || date === 4 || date === 5) && month === 3 && year === 2021) {
+          cell.classList.add("event");
+        }
+        else if (date === 27 && month === 3) {
+          cell.classList.add("event");
+        }
+        // May, June events.
+        else if ((date === 1 && month === 4) || (date === 16 && month === 5)) {
+          cell.classList.add('event');
+        }
+        // July, August, September events.
+        else if ((date === 9 && month === 7) || (year === 2020 && date === 10 && month === 7) || (date === 24 && month === 8)) {
+          cell.classList.add("event");
+        }
+        // December events
+        else if (date === 25 && month === 11) {
+          cell.classList.add("xmas");
+        }
+        else if ((date === 26 || date === 16) && month === 11) {
+          cell.classList.add("event");
         }
         cell.appendChild(cell_txt);
         row.appendChild(cell);
         date++;
       }
     }
-		
-		// Finally, appending each row into calendar body.
+    // Finally, appending each row into calendar body.
     tbl.appendChild(row);
+
   }
+}
+
+//window.addEventListener('touchmove', swipe);
+
+function swipe(event) {
+  var
+    hidden_div = document.querySelector('.hidden-div'),
+    arr = [],
+    arr2 = [];
+    
+  let x = event.touches[0].clientX;
+  arr = [x];
+  for (let i = 0; i < arr.length; i++) {
+    hidden_div.innerHTML += arr + "<br>";
+    arr2 = hidden_div.innerHTML.split('<br>');
+    if (arr2.length < 10) {
+      if (parseInt(arr2[0]) > 200 && parseInt(arr2[5]) < 150 && parseInt(arr2[5]) > 100) {
+        next();
+        break;
+      }
+      else { return false; }
+    }
+    else if (arr2.length > 10) {
+      hidden_div.innerHTML = "";
+    }
+  }
+  setTimeout(swipe, 1000);
 }
--- js/script.js
+++ js/script.js
@@ -1,286 +1,186 @@
-function modulo(num, denom) { if (num%denom >= 0) { return Math.abs(num%denom); } else { return num%denom + denom; } }
 function q(selector) { return document.querySelector(selector); }
 
 function clock() {
-	var
-	date = new Date(),
-	current_year = date.getFullYear(),
-	current_month	 = date.getMonth(),
-	current_day = date.getDay();
-	
-	let year_days = 367 - new Date(current_year, 0, 367).getDate();
-	let mon_days = 32 - new Date(current_year, current_month, 32).getDate();
-	
-	// Constant variables
-	var
-	day_secs = 86400,
-	year_secs = day_secs*year_days,
-	month_secs = year_secs / 12,
-	week_secs = 604800,
-	year_weeks = year_secs / week_secs,
-	hour_secs = 3600,
-	min_secs = 60;	
-	
-	// Calcuting current year's progress and remaining time.
-	var
-	prog = Math.floor(date.getTime() / 1000) - Math.ceil(new Date(current_year-1, 11, 31).getTime() / 1000);
-	rem = Math.ceil(year_secs - prog);
-	
-	var
-	rem_days = Math.floor( rem / day_secs ),
-	rem_weeks = Math.floor( rem / week_secs ),
-	rem_hours = Math.floor( rem / hour_secs ),
-	rem_minutes = Math.floor( rem / min_secs );
-	
-	let fmonth = 12 - Math.ceil(prog / month_secs);
-	let fweeks = rem_weeks - 4*fmonth;
-	let fdays = 1 + rem_days - 7*rem_weeks;
-	let fhours = rem_hours - 24*rem_days;
-	let fminutes = rem_minutes - 60*rem_hours;
-	let fseconds = rem - 60*rem_minutes;
-	
-	// Strings for labels.
-	
-	let pmonths = "months";
-	let smonths = "month";
-	let pdays = "days ";
-	let sdays = "day ";
-	let pweeks = "weeks";
-	let sweeks = "week";
-	let phours = "hours";
-	let shours = "hour";
-	let pminutes = "minutes";
-	let sminutes = "minute";
-	let pseconds = "seconds";
-	let sseconds = "second";
-	
-	// Assigning the DOM to variables.
-	let rem_months_dom = q(".rem-months");
-	let rem_weeks_dom = q(".rem-weeks");
-	let rem_days_dom = q(".rem-days");
-	let rem_hours_dom = q(".rem-hours");
-	let rem_minutes_dom = q(".rem-minutes");
-	let rem_seconds_dom = q(".rem-seconds");
-	
-	let rem_months_label 	= q(".rem-months-label");
-	let rem_weeks_label = q(".rem-weeks-label");
-	let rem_days_label = q(".rem-days-label");
-	let rem_hours_label = q(".rem-hours-label");
-	let rem_minutes_label = q(".rem-minutes-label");
-	let rem_seconds_label = q(".rem-seconds-label");
-	
-	// Setting up conditions for displaying remaining time.
-	if ( fmonth > 0 && fweeks !== 0 && fdays !== 0 ) {
-		rem_months_label.innerHTML = fmonth > 1 ? pmonths : smonths;
-		rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
-		rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-	} 
-	
-	else if ( fmonth > 0 && fweeks !== 0 && fdays === 0 ) {
-		rem_months_label.innerHTML = fmonth > 1 ? pmonths : smonths;
-		rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
-		rem_days_dom.style.display = "none";
-		rem_days_label.style.display = "none";
-	}
-	
-	else if ( fmonth > 0 && fweeks === 0 && fdays !== 0 ) {
-		rem_months_label.innerHTML = fmonth > 1 ? pmonths : smonths;
-		rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-		rem_weeks_dom.style.display = "none";
-		rem_weeks_label.style.display = "none";
-	}
-	
-	else if ( fmonth < 1 && fweeks > 0 ) {
-		rem_months_dom.style.display = "none";
-		rem_months_label.style.display = "none";
-		rem_hours_dom.style.display = "inline";
-		rem_hours_label.style.display = "inline";
-		
-		if (  fweeks !== 0 && fdays !== 0 && fhours !== 0 ) {
-			rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
-			rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-		}
-		
-		else if ( fweeks !== 0 && fdays !== 0 && fhours === 0 ) {
-			rem_hours_dom.style.display = "none";
-			rem_hours_label.style.display = "none";
-		
-			rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
-			rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-		}
-		
-		else if ( fweeks !== 0 && fdays === 0 && fhours !== 0 ) {
-			rem_days_dom.style.display = "none";
-			rem_days_label.style.display = "none";
-		
-			rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-		}
-		
-		else if ( fweeks !== 0 && fdays === 0 && fhours === 0 ) {
-			rem_days_dom.style.display = "none";
-			rem_days_label.style.display = "none";
-			rem_hours_dom.style.display = "none";
-			rem_hours_label.style.display = "none";
-			
-			rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
-		}
-		
-	}
-	
-	else if ( fmonth < 1 && fweeks < 1 ) {
-		rem_months_dom.style.display = "none";
-		rem_months_label.style.display = "none";
-		rem_weeks_dom.style.display = "none";
-		rem_weeks_label.style.display = "none";
-		
-		if ( fdays !== 0 && fhours !== 0 && fminutes !== 0 ) {
-			rem_hours_dom.style.display = "inline";
-			rem_hours_label.style.display = "inline";
-			rem_minutes_dom.style.display = "inline";
-			rem_minutes_label.style.display = "inline";
-			
-			rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-			rem_minutes_label.innerHTML = fminutes > 1 ? pminutes : sminutes;
-		}
-		
-		else if ( fdays !== 0 && fhours !== 0 && fminutes === 0 ) {
-			rem_hours_dom.style.display = "inline";
-			rem_hours_label.style.display = "inline";
-			rem_minutes_dom.style.display = "none";
-			rem_minutes_label.style.display = "none";
-			
-			rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-		}
-		
-		else if ( fdays !== 0 && fhours === 0 && fminutes !== 0 ) {
-			rem_hours_dom.style.display = "none";
-			rem_hours_label.style.display = "none";
-			rem_minutes_dom.style.display = "inline";
-			rem_minutes_label.style.display = "inline";
-			
-			rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-			rem_minutes_label.innerHTML = fminutes > 1 ? pminutes : sminutes;
-		}
-		
-		else if ( fdays !== 0 && fhours === 0 && fminutes === 0 ) {
-			rem_hours_dom.style.display = "none";
-			rem_hours_label.style.display = "none";
-			rem_minutes_dom.style.display = "none";
-			rem_minutes_label.style.display = "none";
-			
-			rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
-		}
-	}
-	
-	else if ( fmonths < 1 && fweeks < 1 && fdays < 1) {
-		rem_months_dom.style.display = "none";
-		rem_months_label.style.display = "none";
-		rem_weeks_dom.style.display = "none";
-		rem_weeks_label.style.display = "none";
-		rem_days_dom.style.display = "none";
-		rem_days_label.style.display = "none";
-		
-		rem_hours_dom.style.display = "inline";
-		rem_hours_label.style.display = "inline";
-		rem_minutes_dom.style.display = "inline";
-		rem_minutes_label.style.display = "inline";
-		rem_seconds_dom.style.display = "inline";
-		rem_seconds_label.style.display = "inline";
-		
-		if ( fhours !== 0 && fminutes !== 0 && fseconds !==0 ) {
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-			rem_minutes_label.innerHTML = fminutes > 1 ? pminutes : sminutes;
-			rem_seconds_label.innerHTML = fseconds > 1 ? pseconds : sseconds;
-		}
-		
-		else if ( fhours !== 0 && fminutes !== 0 && fseconds ===0 ) {
-			rem_seconds_dom.style.display = "none";
-			rem_seconds_label.style.display = "none";
-			
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-			rem_minutes_label.innerHTML = fminutes > 1 ? pminutes : sminutes;
-		}
-		
-		else if ( fhours !== 0 && fminutes === 0 && fseconds !==0 ) {
-			rem_minutes_dom.style.display = "none";
-			rem_minutes_label.style.display = "none";
-			
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-			rem_seconds_label.innerHTML = fseconds > 1 ? pseconds : sseconds;
-		}
-		
-		else if ( fhours !== 0 && fminutes === 0 && fseconds ===0 ) {
-			rem_minutes_dom.style.display = "none";
-			rem_minutes_label.style.display = "none";
-			rem_seconds_dom.style.display = "none";
-			rem_seconds_label.style.display = "none";
-			
-			rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
-		}
-		
-		else if ( fhours < 1 && fminutes !== 0 && fseconds !== 0 ) {
-			rem_hours_dom.style.display = "none";
-			rem_hours_label.style.display = "none";
-			
-			rem_minutes_label.innerHTML = fminutes > 1 ? pminutes : sminutes;
-			rem_seconds_label.innerHTML = fseconds > 1 ? pseconds : sseconds;
-		}
-		
-		else if ( fhours < 1 && fminutes < 1 && fseconds !== 0 ) {
-			rem_hours_dom.style.display = "none";
-			rem_hours_label.style.display = "none";
-			rem_minutes_dom.style.display = "none";
-			rem_minutes_label.style.display = "none";
-			rem_seconds_dom.style.width = "50px";
-			rem_seconds_dom.style.height = "50px";
-			rem_seconds_dom.style.fontSize = "35px";
-			
-			rem_seconds_label.innerHTML = fseconds > 1 ? pseconds : sseconds;
-		}
-	}
-	
-	// Inflating the DOM with remaining time.
-	rem_months_dom.innerHTML = fmonth;
-	rem_weeks_dom.innerHTML = fweeks;
-	rem_days_dom.innerHTML = fdays;
-	rem_hours_dom.innerHTML = fhours;
-	rem_minutes_dom.innerHTML = fminutes;
-	rem_seconds_dom.innerHTML = fseconds;
-	
-	// Setting up a progress bar for the loading year.
-	let next_year = q(".year-progress");
-	next_year.innerHTML = "<div class='progress-bar'></div>";
-	
-	let disp_year = q(".from-to-year");
-	disp_year.innerHTML = "<span class='dying-year'>" + current_year + "</span>&nbsp;&nbsp;>&nbsp;&nbsp;" + "<span class='pending-year'>" + parseInt(current_year + 1) + "</span>";
-	
-	let progress_count = q(".progress-count");
-	let fprogress = (100*((prog/day_secs) / year_days)).toFixed(1);
-	let fprog_string = fprogress.toString();
-	progress_count.innerHTML = (fprog_string.charAt(fprog_string.length-1) == "0" ) ? Math.floor(fprogress) + "% COMPLETE" : fprogress + "% COMPLETE";
-	
-	let fade_away = q(".dying-year");
-	let fade_in = q(".pending-year");
-	
-	fade_away.style.opacity = 1.1 - (fprogress/100).toFixed(1);
-	fade_in.style.opacity = 1.1 - ((100-fprogress)/100).toFixed(1);
-	
-	fade_away.style.filter = "blur(" + ((2*fprogress)/100).toFixed(1) + "px)";
-	fade_in.style.filter = "blur(" + ((2*(100-fprogress))/100).toFixed(1) + "px)";
-	
-	let progress_bar_width = q(".progress-bar");
-	let fprog_width = (165 * (1 - rem_days/year_days));
-	fprog_width = ( Math.ceil(fprog_width) - fprog_width) < 0.5 ? Math.ceil(fprog_width) : Math.floor(fprog_width);
-	progress_bar_width.style.width = fprog_width + "px";
-	
-	let today = ("<span class='day'>" + (date.toString().split(' ')[0]).bold() + "</span>" + ' ' + (date.toString().split(' ').splice(1,2).join(' ') + ', ' + current_year)).toUpperCase();
-	q(".time-disp").innerHTML = "Day <b>" + Math.floor(prog / day_secs) + "</b> of " + year_days;
-	q(".date-disp").innerHTML = today;
-	setInterval(clock, 1000);
+  let date = new Date();
+  let current_year = date.getFullYear();
+  let current_month = date.getMonth();
+  let current_day = date.getDate();
+
+  var getDaysInMonth = function(month, year) {
+    return new Date(year, month + 1, 0).getDate();
+  };
+
+  let year_days = 367 - new Date(current_year, 0, 367).getDate();
+  let mon_days = 32 - new Date(current_year, current_month, 32).getDate();
+
+  // Constant variables
+  let day_secs = 86400;
+  let week_secs = 7 * day_secs;
+  let hour_secs = 3600;
+  let min_secs = 60;
+
+  // Calcuting current year's progress and remaining time.
+  let prog = ((date.getTime() / 1000) - (new Date(current_year - 1, 11, 31, 0, 0, 0).getTime() / 1000)).toFixed(0);
+  let rem = (((new Date(current_year, 11, 31, 0, 0, 0).getTime() / 1000) - (new Date(current_year - 1, 11, 31, 0, 0, 0).getTime() / 1000)).toFixed(0)) - prog;
+
+  let rem_days = getDaysInMonth(current_month, current_year) - current_day;
+  //let rem_weeks = (rem / week_secs);
+  let rem_hours = Math.floor(rem / hour_secs);
+  let rem_minutes = Math.floor(rem / min_secs);
+
+  let fmonths = 12 - (current_month + 1);
+  let fdays = rem_days % 7;
+  let fweeks = Math.floor(rem_days / 7);
+  let fhours = rem_hours % 24;
+  let fminutes = rem_minutes % 60;
+  let fseconds = rem % 60;
+
+  // Strings for labels.
+
+  let pmonths = "months";
+  let smonths = "month";
+  let pdays = "days ";
+  let sdays = "day ";
+  let pweeks = "weeks";
+  let sweeks = "week";
+  let phours = "hours";
+  let shours = "hour";
+  let pminutes = "minutes";
+  let sminutes = "minute";
+  let pseconds = "seconds";
+  let sseconds = "second";
+
+  // Assigning the DOM to variables.
+
+  let rem_months_dom = q(".rem-months");
+  let rem_weeks_dom = q(".rem-weeks");
+  let rem_days_dom = q(".rem-days");
+  let rem_hours_dom = q(".rem-hours");
+  let rem_minutes_dom = q(".rem-minutes");
+  let rem_seconds_dom = q(".rem-seconds");
+
+  let rem_months_label = q(".rem-months-label");
+  let rem_weeks_label = q(".rem-weeks-label");
+  let rem_days_label = q(".rem-days-label");
+  let rem_hours_label = q(".rem-hours-label");
+  let rem_minutes_label = q(".rem-minutes-label");
+  let rem_seconds_label = q(".rem-seconds-label");
+
+  // Setting up conditions for displaying remaining time.
+
+  rem_weeks_dom.style.display = (fweeks === 0) ? "none" : "inline";
+  rem_weeks_label.style.display = (fweeks === 0) ? "none" : "inline";
+
+  rem_days_dom.style.display = (fdays === 0) ? "none" : "inline";
+  rem_days_label.style.display = (fdays === 0) ? "none" : "inline";
+
+  rem_months_dom.style.display = (fmonths === 0) ? "none" : "inline";
+  rem_months_label.style.display = (fmonths === 0) ? "none" : "inline";
+
+  rem_hours_dom.style.display = (fhours === 0) ? "none" : "inline";
+  rem_hours_label.style.display = (fhours === 0) ? "none" : "inline";
+
+  rem_minutes_dom.style.display = (fminutes === 0) ? "none" : "inline";
+  rem_minutes_label.style.display = (fminutes === 0) ? "none" : "inline";
+
+  rem_seconds_dom.style.display = (fseconds === 0) ? "none" : "inline";
+  rem_seconds_label.style.display = (fseconds === 0) ? "none" : "inline";
+
+
+  if (fmonths > 0) {
+    rem_seconds_dom.style.display = "none";
+    rem_minutes_dom.style.display = "none";
+    rem_hours_dom.style.display = "none";
+
+    rem_months_label.innerHTML = fmonths > 1 ? pmonths : smonths;
+    rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
+    rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
+  }
+
+  else if (fmonths < 1 && fweeks > 0) {
+    rem_seconds_dom.style.display = "none";
+    rem_minutes_dom.style.display = "none";
+
+    rem_weeks_label.innerHTML = fweeks > 1 ? pweeks : sweeks;
+    rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
+    rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
+  }
+
+  else if (fmonths < 1 && fweeks < 1) {
+    rem_minutes_dom.style.display = "none";
+
+    rem_days_label.innerHTML = fdays > 1 ? pdays : sdays;
+    rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
+    rem_minutes_label.innerHTML = fminutes > 1 ? pminutes : sminutes;
+  }
+
+  else if (fmonths < 1 && fweeks < 1 && fdays < 1) {
+    rem_hours_label.innerHTML = fhours > 1 ? phours : shours;
+    rem_minutes_label.innerHTML = fminutes > 1 ? pminutes : sminutes;
+    rem_seconds_label.innerHTML = fseconds > 1 ? pseconds : sseconds;
+  }
+
+  // Inflating the DOM with remaining time.
+  rem_months_dom.innerHTML = fmonths;
+  rem_weeks_dom.innerHTML = fweeks;
+  rem_days_dom.innerHTML = fdays;
+  rem_hours_dom.innerHTML = fhours;
+  rem_minutes_dom.innerHTML = fminutes;
+  rem_seconds_dom.innerHTML = fseconds;
+
+  // Setting up a progress bar for the loading year.
+  let next_year = q(".year-progress");
+  next_year.innerHTML = "<div class='progress-bar'></div>";
+
+  let disp_year = q(".from-to-year");
+  disp_year.innerHTML = `<span class='dying-year'>${current_year}</span>&nbsp;&nbsp;>&nbsp;&nbsp;<span class='pending-year'>${parseInt(current_year + 1)}</span>`;
+
+  let progress_count = q(".progress-count");
+  let fprogress = (100 * ((prog / day_secs) / year_days)).toFixed(1);
+  let fprog_string = fprogress.toString();
+  progress_count.innerHTML = (fprog_string.charAt(fprog_string.length - 1) == "0") ? Math.floor(fprogress) + "% COMPLETE" : fprogress + "% COMPLETE";
+
+  let fade_away = q(".dying-year");
+  let fade_in = q(".pending-year");
+
+  fade_away.style.opacity = 1.1 - (fprogress / 100).toFixed(1);
+  fade_in.style.opacity = 1.1 - ((100 - fprogress) / 100).toFixed(1);
+
+  fade_away.style.color = (current_year === 2021) ? "#BDB220" : "#EBEBEB";
+  
+  fade_away.style.filter = `blur(${((2 * fprogress) / 100).toFixed(1)}px)`;
+  fade_in.style.filter = `blur(${((2 * (100 - fprogress)) / 100).toFixed(1)}px)`;
+
+  let progress_bar = q(".progress-bar");
+  let fprog_width = ((165 * (1 - (rem / day_secs) / year_days))).toFixed(0);
+  fprog_width = (Math.ceil(fprog_width) - fprog_width) < 0.5 ? Math.ceil(fprog_width) : Math.floor(fprog_width);
+  progress_bar.style.width = `${fprog_width}px`;
+  let R;
+  let G;
+  if (fprogress <= 25) {
+    R = (((4 * fprogress) / 100) * (255 / 2)).toFixed(0);
+    progress_bar.style.backgroundColor = `rgba(${R}, 255, 75)`;
+    progress_count.style.color = `rgba(${R}, 255, 75)`;
+  }
+
+  else if (fprogress > 25 && fprogress <= 50) {
+    R = (((2 * fprogress) / 100) * (255)).toFixed(0);
+    progress_bar.style.backgroundColor = `rgba(${R}, 255, 75)`;
+    progress_count.style.color = `rgba(${R}, 255, 75)`;
+  }
+
+  else if (fprogress > 50 && fprogress <= 100) {
+    G = ((2 * (100 - fprogress) / 100) * (255)).toFixed(0);
+    progress_bar.style.backgroundColor = `rgba(255, ${G}, 75)`;
+    progress_count.style.color = `rgba(255, ${G}, 75)`;
+  }
+
+  let day_span = "<span class='day'>";
+  let progress_day =  Math.floor(prog / day_secs);
+  let today = day_span + ((date.toString().split(' ')[0]).bold() + ", </span> " + (date.toString().split(' ').splice(1, 1).join(' ') + ` ${current_day}`)).toUpperCase();
+  q(".time-disp").innerHTML = `Day <b>${progress_day}</b> of ${year_days}`;
+  q(".date-disp").innerHTML = today;
+  setInterval(clock, 1000);
 }
 
 clock();
